1766af40893cf6b3d92e623d66995caa0282c36c
Menernos <brandon-eg@hotmail.com>
2021-07-07 00:32:55 -0600

    Primer commit.

---
+++ app/javascript/channels/comments_channel.js
@@ -0,0 +1,16 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("CommentsChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    let comments = document.getElementById('comments');
+    comments.innerHTML += data.comment;
+  }
+});
